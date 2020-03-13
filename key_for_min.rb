d3222c6c9f9b98beaede15cc29df020f7f496b02
John Michael Robey <robey14@gmail.com>
2020-03-13 16:29:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,6 +13,6 @@ lowest_value = 0
   lowest_key
 end
 
-ikea = {:chair => 25, :table => 85, :mattress => 450}
-key_for_min_value(ikea)
+furniture = {:chair => 25, :table => 85, :mattress => 450}
+key_for_min_value(furniture)
 
